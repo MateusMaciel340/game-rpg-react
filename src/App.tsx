fa9130043fd07d19121e2b1a998064157a769c75
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-12-14 13:42:40 -0300

    feat: central and secondary components | types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,56 @@
+import { useEffect, useState } from "react";
+import * as C from "./styles/AppStyle";
+import { Character } from "./components/Character/Character";
+import { useCharacter } from "./hooks/useCharacter";
+
+import "./styles/AppStyle.css";
+
+
 const App = () => {
+
+    const char = useCharacter("Mateus");
+    const char2 = useCharacter("Maxwell");
+
+    useEffect(() => {
+        window.addEventListener("keydown", handleKeyDown);
+    }, []);
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+        switch (e.code) {
+            case "KeyA": 
+            case "ArrowLeft":
+                char.moveLeft();
+            break;
+            
+            case "KeyW":
+            case "ArrowUp":
+                char.moveUp();
+            break;
+            
+            case "KeyD":
+            case "ArrowRight":
+                char.moveRight();
+            break;
+            
+            case "KeyS":
+            case "ArrowDown":
+                char.moveDown();
+            break;
+        }
+    }
+
     return (
-        <h1>Hello World!</h1>
+        <C.Container>
+            <C.Map>
+                <Character
+                    x={char.x} y={char.y}
+                    side={char.side} name={char.name} />
+                
+                <Character
+                    x={char2.x} y={char2.y}
+                    side={char2.side} name={char2.name}/>
+            </C.Map>
+        </C.Container>
     );
 }
 
